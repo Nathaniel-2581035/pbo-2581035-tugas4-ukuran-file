2f1f2db7312da6a6e81c0f3e382d82ff8fea9689
Nathaniel Adelio <Nathan@unai.edu>
2026-09-20 11:23:59 +0700

    penambahan 3 komentar

--- src/UkuranFile.java
+++ src/UkuranFile.java
@@ -10,16 +10,17 @@ public class UkuranFile {
         String namaFile = input.nextLine();
 
         System.out.print("Ukuran (byte) : ");
-        long ukuranByte = input.nextLong();
+        long ukuranByte = input.nextLong(); //pake long karena kapasitasnya lebih besar dari int
 
         double ukuranKB = ukuranByte / SATU_KB;
         double ukuranMB = ukuranKB / SATU_KB;
         double ukuranGB = ukuranMB / SATU_KB;
 
+        //perhitungannya harus presisi maka kita buat pake double biar perhitungannya presisi sehingga jika ad desimal maka desimal terbaca
         System.out.println("Ukuran (KB): " + ukuranKB);
         System.out.println("Ukuran (MB): " + ukuranMB);
         System.out.println("Ukuran (GB): " + ukuranGB);
-        int ukuranMBDibulatkan = (int) ukuranMB;
+        int ukuranMBDibulatkan = (int) ukuranMB; //pake int karna untuk ngebuang desimal bukan buat buletin angknya
         System.out.println("Dibulatkan ke MB : " + ukuranMBDibulatkan);
         double selisihPembulatan = ukuranMB - ukuranMBDibulatkan;
         System.out.println("Selisih pembulatan: " + selisihPembulatan);
